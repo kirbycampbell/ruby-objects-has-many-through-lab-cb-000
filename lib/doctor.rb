8f265407f9d11d3a9b31545cc5837673e7c56c1b
Kirby Campbell <jkirbycampbell@gmail.com>
2018-05-10 19:07:54 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,24 @@
 class Doctor
 
+attr_accessor :name
 
+  def initialize(name)
+    @name = name
+    @appointments = []  
+  end
+
+  def add_appointment(appt)
+    @appointments << appt
+    appt.doctor = self
+  end
+
+  def appointments
+    @appointments
+  end
+
+  def patients
+    self.appointments.collect do |patient|
+      patient.doctor
+  end
 
 end
